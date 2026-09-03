2671ffd70953848b60900b09613cc9f447b55318
hasti2 <hastimanooch84@gmail.com>
2026-09-03 15:38:52 +0330

    fix: allow owners to access all agency properties

--- backend/properties/selector/property_selector.py
+++ backend/properties/selector/property_selector.py
@@ -5,7 +5,6 @@ class PropertySelector:
 
     @staticmethod
     def all(user):
-
         qs = (
             Property.objects.select_related(
                 "agency",
@@ -19,22 +18,24 @@ class PropertySelector:
         )
 
         if not user.is_owner:
-            qs = qs.filter(
-                address__neighborhood__in=user.service_neighborhoods.all()
-            )
+            qs = qs.filter(agent=user)
 
         return qs
 
     @staticmethod
     def by_id(pk, user):
-
-        return Property.objects.select_related(
+        qs = Property.objects.select_related(
             "agency",
             "owner",
             "agent",
             "create_by",
             "address",
-        ).get(
+        ).filter(
             pk=pk,
             agency=user.agency,
         )
+
+        if not user.is_owner:
+            qs = qs.filter(agent=user)
+
+        return qs.get()
